d5fb3af58613df1acf83a2bde4406e95e12be76a
Fraser Kearns <kearnsmain@gmail.com>
2017-03-05 09:23:30 +1300

    Adding favicon.

--- build/routes.js
+++ build/routes.js
@@ -30,7 +30,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var router = _express2.default.Router();
 
-var unmanagedMarkup = '<title>Spell List</title><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/><link rel = "stylesheet" type = "text/css" href = "styles/style.css"/><script>window.__INITIAL_STORE__ = ' + JSON.stringify(_configureStore.store) + '; </script>';
+var unmanagedMarkup = '<title>Spell List</title><link rel = "shortcut icon" href = "images/favicon.ico"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/><link rel = "stylesheet" type = "text/css" href = "styles/style.css"/><script>window.__INITIAL_STORE__ = ' + JSON.stringify(_configureStore.store) + '; </script>';
 
 var routes = (0, _reactRouter.createRoutes)((0, _router2.default)());
 
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import {RouterContext, createRoutes, match} from "react-router";
 
 let router = Express.Router();
 
-let unmanagedMarkup = '<title>Spell List</title><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/><link rel = "stylesheet" type = "text/css" href = "styles/style.css"/><script>window.__INITIAL_STORE__ = ' + JSON.stringify(store) + '; </script>';
+let unmanagedMarkup = '<title>Spell List</title><link rel = "shortcut icon" href = "images/favicon.ico"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/><link rel = "stylesheet" type = "text/css" href = "styles/style.css"/><script>window.__INITIAL_STORE__ = ' + JSON.stringify(store) + '; </script>';
 
 const routes = createRoutes(AppRouter());
 
