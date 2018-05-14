4997bbc1b03b06aa518e7c2037bfad5d573b3a96
Fman72 <kearnsmain@gmail.com>
2018-05-14 18:23:58 +1200

    Making dynamo work

--- src/databaseRoutes.js
+++ src/databaseRoutes.js
@@ -1,16 +1,20 @@
 import Express from 'express';
 import AWSSDK from 'aws-sdk';
+import {dynamoDbEndpoint, dynamoDbAccessKey, dynamoDbSecret} from "./sensitiveStuff";
 
+let documentClient;
 
 if(process.env.DYNAMODB_ENDPOINT){
   console.log("LIVE ENV DETECTED");
-  AWSSDK.config.update({region: 'ap-southeast-2', endpoint: process.env.DYNAMODB_ENDPOINT}); //, accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
+  AWSSDK.config.update({region: 'ap-southeast-2', endpoint: process.env.DYNAMODB_ENDPOINT, });
+}
+else if (dynamoDbEndpoint){
+  documentClient = new AWSSDK.DynamoDB.DocumentClient({region: 'ap-southeast-2', endpoint: dynamoDbEndpoint, secretAccessKey: dynamoDbSecret, accessKeyId: dynamoDbAccessKey});
 }
 else{
   console.log("LOCAL ENV DETECTED");
   AWSSDK.config.update({region: 'ap-southeast-2', endpoint: 'http://localhost:8000'});
 }
-let documentClient = new AWSSDK.DynamoDB.DocumentClient();
 //documentClient.setEndpoint('http://127.0.0.1:8000');
 let databaseRouter = Express.Router();
 
